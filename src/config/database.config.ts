80996193610c955a2805a2aac01cb5186091550f
478Dell <n.anikin@softteco.com>
2023-02-26 16:38:01 +0300

    deploy

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -12,58 +12,61 @@ import { Homework } from "../homework/homework.entity";
 import { Topic } from "../topic/topic.entity";
 
 enum Env {
-    prod = "production",
-    dev = "development",
-    test = "testing",
+  prod = "production",
+  dev = "development",
+  test = "testing",
 }
 
 function getDbConfig(
   configService: ConfigService,
   entities: any[]
 ): PostgresConnectionOptions {
-    const env = configService.get<Env>("NODE_ENV");
+  const env = configService.get<Env>("NODE_ENV");
 
-    switch (env) {
-        case Env.prod:
-            return {
-                synchronize: false,
-                type: configService.get("DB_TYPE"),
-                host: configService.get("DB_HOST"),
-                username: configService.get("DB_USER"),
-                password: configService.get("DB_PASS"),
-                database: configService.get("DB_NAME"),
-                entities,
-                migrations: [ __dirname + "/migrations/*.ts" ],
-                migrationsTableName: "migrations"
-            };
-        default:
-            return {
-                synchronize: true,
-                type: configService.get("DB_TYPE"),
-                port: configService.get<number>("DB_PORT"),
-                host: configService.get("DB_HOST"),
-                username: configService.get("DB_USERNAME"),
-                password: configService.get("DB_PASSWORD"),
-                database: configService.get("DB_DATABASE"),
-                entities,
-                migrations: [ __dirname + "/migrations/*.ts" ],
-                migrationsTableName: "migrations"
-            };
-    }
+  switch (env) {
+    case Env.prod:
+      return {
+        synchronize: false,
+        type: configService.get("DB_TYPE"),
+        host: configService.get("DB_HOST"),
+        username: configService.get("DB_USERNAME"),
+        password: configService.get("DB_PASSWORD"),
+        database: configService.get("DB_DATABASE"),
+        entities,
+        migrations: [ __dirname + "/migrations/*.ts" ],
+        migrationsTableName: "migrations"
+      };
+    default:
+      return {
+        synchronize: true,
+        type: configService.get("DB_TYPE"),
+        port: configService.get<number>("DB_PORT"),
+        host: configService.get("DB_HOST"),
+        username: configService.get("DB_USERNAME"),
+        password: configService.get("DB_PASSWORD"),
+        database: configService.get("DB_DATABASE"),
+        entities,
+        ssl: {
+          rejectUnauthorized: false
+        },
+        migrations: [ __dirname + "/migrations/*.ts" ],
+        migrationsTableName: "migrations"
+      };
+  }
 }
 
 export const DatabaseModule = TypeOrmModule.forRootAsync({
-    imports: [ ConfigModule ],
-    inject: [ ConfigService ],
-    useFactory: (configService: ConfigService) =>
-      getDbConfig(configService, [
-          User,
-          Student,
-          Group,
-          Teacher,
-          School,
-          Lesson,
-          Homework,
-          Topic,
-      ])
+  imports: [ ConfigModule ],
+  inject: [ ConfigService ],
+  useFactory: (configService: ConfigService) =>
+    getDbConfig(configService, [
+      User,
+      Student,
+      Group,
+      Teacher,
+      School,
+      Lesson,
+      Homework,
+      Topic
+    ])
 });
